a397412563899463131085a0dfdd6bf11abc41ca
zsnyder21 <64924773+zsnyder21@users.noreply.github.com>
2021-07-13 11:17:15 -0600

    Reworked how the random sentences are generated

--- DataGenerator.py
+++ DataGenerator.py
@@ -5,7 +5,7 @@ import random
 
 
 class DataGenerator(object):
-    def __init__(self, imageSize: int, outputDirectory: str, greyScale: bool = True):
+    def __init__(self, imageSize: int, outputDirectory: str, greyScale: bool = True, dictionaryLength: int = 200):
         if greyScale:
             self.imageSize = (imageSize, imageSize)
         else:
@@ -13,42 +13,48 @@ class DataGenerator(object):
 
         self.outputDirectory = outputDirectory
 
-        self.codePointRanges = [
-            (0x0021, 0x0021),
-            (0x0023, 0x0026),
-            (0x0028, 0x007E),
-            (0x00A1, 0x00AC),
-            (0x00AE, 0x00FF),
-            (0x0100, 0x017F),
-            (0x0180, 0x024F),
-            (0x2C60, 0x2C7F),
-            (0x16A0, 0x16F0),
-            (0x0370, 0x0377),
-            (0x037A, 0x037E),
-            (0x0384, 0x038A),
-            (0x038C, 0x038C),
-        ]
+        self.dictionaryLength = dictionaryLength
 
-        self.alphabet = [
-            chr(code_point) for current_range in self.codePointRanges
-            for code_point in range(current_range[0], current_range[1] + 1)
-        ]
+        # self.codePointRanges = [
+        #     (0x0021, 0x0021),
+        #     (0x0023, 0x0026),
+        #     (0x0028, 0x007E),
+        #     (0x00A1, 0x00AC),
+        #     (0x00AE, 0x00FF),
+        #     (0x0100, 0x017F),
+        #     (0x0180, 0x024F),
+        #     (0x2C60, 0x2C7F),
+        #     (0x16A0, 0x16F0),
+        #     (0x0370, 0x0377),
+        #     (0x037A, 0x037E),
+        #     (0x0384, 0x038A),
+        #     (0x038C, 0x038C),
+        # ]
+        #
+        # self.alphabet = [
+        #     chr(code_point) for current_range in self.codePointRanges
+        #     for code_point in range(current_range[0], current_range[1] + 1)
+        # ]
 
     def generateImage(self) -> np.array:
         return np.random.randint(0, 256, self.imageSize) / 255.0
 
+    def oneHotEncode(self, array: np.array) -> np.array:
+        maxValue = self.dictionaryLength
+        return np.eye(maxValue)[array]
+
     def generateSentence(self, sentenceLength: int) -> np.array:
-        return np.array([ord(np.random.choice(self.alphabet)) for _ in range(sentenceLength)])
+        return np.array([np.random.randint(low=0, high=self.dictionaryLength) for _ in range(sentenceLength)]).reshape(1, -1)
 
     def messageEncode(self, message: str) -> np.array:
-        return np.array([ord(char) for char in message])
+        return np.array([ord(char) for char in message]).reshape(1, -1)
 
     def messageDecode(self, message: np.array) -> str:
-        return "".join(chr(codePoint) for codePoint in message)
+        return "".join(chr(codePoint) for codePoint in message[0])
 
 
 if __name__ == "__main__":
-    generator = DataGenerator(imageSize=300, greyScale=False, outputDirectory="Some Fake Place")
+    generator = DataGenerator(imageSize=300, greyScale=False, dictionaryLength=200, outputDirectory="Some Fake Place")
     # img = generator.generateImage()
 
     sentence = generator.generateSentence(100)
@@ -58,6 +64,8 @@ if __name__ == "__main__":
     sentenceEncoded = generator.messageEncode(sentenceDecoded)
     print(sentenceEncoded)
 
+    test = generator.oneHotEncode(sentence[0])
+    print(test[0], len(test[0])) #, len(test[0]))
     # fig, ax = plt.subplots(figsize=(10,10))
     # ax.imshow(img, cmap="gray")
     # plt.show()
