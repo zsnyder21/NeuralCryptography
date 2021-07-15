00935785e17f664885e078d63ebbeb4e5354971b
zsnyder21 <64924773+zsnyder21@users.noreply.github.com>
2021-07-15 16:42:54 -0600

    Added functionality to pickup a model where it was leftoff at

--- src/ModelTrainer.py
+++ src/ModelTrainer.py
@@ -12,7 +12,8 @@ class ModelTrainer(object):
                  imageSize: int = 100,
                  greyScale: bool = True,
                  dictionaryLength: int = 200,
-                 batchSize: int = 32):
+                 batchSize: int = 32,
+                 loadExistingModel: bool = False):
         """
         This class is responsible for training a model to encrypt/decrypt
         string information within an image. This method will save the parameters
@@ -23,47 +24,66 @@ class ModelTrainer(object):
         :param greyScale: Whether or not the images will be greyscale
         :param dictionaryLength: Number of distinct characters to use within sentences
         :param batchSize: Number of images used per batch when training
+        :param loadExistingModel: Whether or not to load an existing model
         """
         self.modelSavePath = modelSavePath
+        self.loadExistingModel = loadExistingModel
 
-        if greyScale:
-            self.imageSize = (imageSize, imageSize, 1)
+        if self.loadExistingModel:
+            file = open(f"{self.modelSavePath}.p", "rb")
+            modelParameters = pickle.load(file=file)
+            file.close()
+
+            self.imageSize = modelParameters["imageSize"]
+            self.greyScale = modelParameters["greyScale"]
+            self.sentenceLength = modelParameters["sentenceLength"]
+            self.dictionaryLength = modelParameters["dictionaryLength"]
+            self.batchSize = modelParameters["batchSize"]
+
+            if self.greyScale:
+                self.imageSize = (self.imageSize, self.imageSize, 1)
+            else:
+                self.imageSize = (self.imageSize, self.imageSize, 3)
+
         else:
-            self.imageSize = (imageSize, imageSize, 3)
+            if greyScale:
+                self.imageSize = (imageSize, imageSize, 1)
+            else:
+                self.imageSize = (imageSize, imageSize, 3)
 
-        self.sentenceLength = imageSize
-        self.dictionaryLength = dictionaryLength
-        self.batchSize = batchSize
+            self.sentenceLength = imageSize
+            self.dictionaryLength = dictionaryLength
+            self.batchSize = batchSize
+
+            modelParameters = {
+                "imageSize": imageSize,
+                "greyScale": greyScale,
+                "sentenceLength": imageSize,
+                "dictionaryLength": dictionaryLength,
+                "batchSize": batchSize
+            }
+
+            file = open(f"{modelSavePath}.p", "wb")
+            pickle.dump(obj=modelParameters, file=file)
+            file.close()
 
         self.modelGenerator = ModelGenerator(
-            imageSize=imageSize,
-            greyScale=greyScale,
-            dictionaryLength=dictionaryLength
+            imageSize=self.imageSize[0],
+            greyScale=self.greyScale,
+            dictionaryLength=self.dictionaryLength
         )
 
         self.dataGenerator = DataGenerator(
-            imageSize=imageSize,
-            greyScale=greyScale,
-            dictionaryLength=dictionaryLength
+            imageSize=self.imageSize[0],
+            greyScale=self.greyScale,
+            dictionaryLength=self.dictionaryLength
         )
 
         self.model = None,
         self.encoder = None,
         self.decoder = None
 
-        modelParameters = {
-            "imageSize": imageSize,
-            "greyScale": greyScale,
-            "sentenceLength": imageSize,
-            "dictionaryLength": dictionaryLength,
-            "batchSize": batchSize
-        }
-
-        file = open(f"{modelSavePath}.p", "wb")
-        pickle.dump(obj=modelParameters, file=file)
-        file.close()
-
-    def trainModels(self, epochs: int, stepsPerEpoch: int, verbose: int = 1):
+    def trainModels(self, epochs: int, stepsPerEpoch: int, verbose: int = 1) -> None:
         """
         This method is responsible for training the models
 
@@ -74,6 +94,9 @@ class ModelTrainer(object):
         """
         dataGenerator = self.dataGenerator.generateData(batchSize=self.batchSize)
         self.model, self.encoder, self.decoder = self.modelGenerator.getModel()
+
+        if self.loadExistingModel:
+            self.model.load_weights(filepath=self.modelSavePath)
 
         self.model.fit(
             x=dataGenerator,
@@ -135,11 +158,12 @@ class EarlyStoppingThreshold(Callback):
 
 if __name__ == "__main__":
     trainer = ModelTrainer(
-        modelSavePath="../data/ModelWeights/someRandomModelTest.h5",
+        modelSavePath="../data/ModelWeights/pickup.h5",
         imageSize=2000,
         greyScale=False,
         dictionaryLength=1000,
-        batchSize=6
+        batchSize=4,
+        loadExistingModel=True
     )
 
     trainer.trainModels(
